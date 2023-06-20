4e13d6deb0de40f6db8b3ce52e93ab229868e497
Zac <a19860101@gmail.com>
2023-06-20 19:30:06 +0800

    20230620

--- response.php
+++ response.php
@@ -20,17 +20,40 @@
     }
 
     extract($_REQUEST);
-    $user = check($user);
-    var_dump($user);
+    if($user == ''){
+        echo '請輸入帳號';
+        echo '<br>';
+    }else{
+        $user = check($user);
+        echo '帳號:'.$user;
+        echo '<br>';
+    }
+    if($pw == ''){
+        echo '請輸入密碼';
+        echo '<br>';
+    }else{
+        $pw = check($pw);
+        echo '密碼:'.$pw;
+        echo '<br>';
+    }
+    echo '生日:'.$birth;
+    echo '<br>';
+    if(!isset($gender)){
+        echo '請輸入性別';
+        echo '<br>';
+    }else{
+        echo '性別:'.$gender;
+        echo '<br>';
+    }
+    /*
+        if(!$gender)
+        if(!isset($gender))
+        if($gender == null)
+        if($gender == '')
+        if(empty($gender))
+    */
 
-    // echo '帳號:'.$user;
-    // echo '<br>';
-    // echo '密碼:'.$pw;
-    // echo '<br>';
-    // echo '生日:'.$birth;
-    // echo '<br>';
-    // echo '性別:'.$gender;
-    // echo '<br>';
-    // echo '學歷:'.$edu;
-    // echo '<br>';
-    // echo '興趣:'.implode(',',$hobby);
+   
+    echo '學歷:'.$edu;
+    echo '<br>';
+    echo '興趣:'.implode(',',$hobby);
