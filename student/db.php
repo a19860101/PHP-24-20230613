9c97be07b94a9f1c22c5512eac793eaa923340cf
Zac <a19860101@gmail.com>
2023-06-27 19:14:42 +0800

    20230627

--- student/db.php
+++ student/db.php
@@ -1,11 +1,16 @@
 <?php
     $db_host = 'localhost';
-    $db_user = 'admin';
-    $db_pw = 'admin';
+    $db_user = 'root';
+    $db_pw = 'root';
     $db_name = 'php-24-20230613';
     $db_charset = 'utf8mb4';
 
     // data source name
     $dsn = "mysql:host={$db_host};dbname={$db_name};charset={$db_charset}";
 
-    $pdo = new PDO($dsn,$db_user,$db_pw);
+    try{
+        $pdo = new PDO($dsn,$db_user,$db_pw);
+    }catch(PDOException $e){
+        // var_dump($e);
+        echo $e->getMessage();
+    }
