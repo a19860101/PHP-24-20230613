47603720d179486696e4e14c5e1034d2da2a09b3
Zac <a19860101@gmail.com>
2023-06-27 19:30:54 +0800

    20230627

--- student/db.php
+++ student/db.php
@@ -1,7 +1,7 @@
 <?php
     $db_host = 'localhost';
     $db_user = 'root';
-    $db_pw = 'root';
+    $db_pw = 'root123';
     $db_name = 'php-24-20230613';
     $db_charset = 'utf8mb4';
 
@@ -10,6 +10,16 @@
 
     try{
         $pdo = new PDO($dsn,$db_user,$db_pw);
+ 
+        // 不主動報錯
+        // $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_SILENT);
+
+        // 主動報錯
+        // $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
+
+        // 主動例外
+        // $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
     }catch(PDOException $e){
         // var_dump($e);
         echo $e->getMessage();
