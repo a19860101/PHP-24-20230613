80868bcd935a9e6df2815fbd924db8e9d8251d47
Zac <a19860101@gmail.com>
2023-07-04 20:55:54 +0800

    20230704

--- student/delete.php
+++ student/delete.php
@@ -1,6 +1,18 @@
 <?php
     include('db.php');
     extract($_REQUEST);
+
+    $sql_avatar = 'SELECT avatar FROM students WHERE id = ?';
+    $stmt = $pdo->prepare($sql_avatar);
+    $stmt->execute([$id]);
+
+    $student = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+    if($student['avatar']!=null){
+        unlink('images/'.$student['avatar']);
+    }
+
+
     $sql = 'DELETE FROM students WHERE id = ?';
     $stmt = $pdo->prepare($sql);
     $stmt->execute([$id]);
