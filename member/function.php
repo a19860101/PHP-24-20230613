a067cae821ee4eeb4ce45fc4b3b6d1be96d242d8
Zac <a19860101@gmail.com>
2023-07-06 20:43:00 +0800

    20230706

--- member/function.php
+++ member/function.php
@@ -4,6 +4,19 @@
     function store($request){
         extract($request);
         $sql = 'INSERT INTO users(user,password,created_at)VALUES(?,?,?)';
-        $stmt = pdo()->prepare($sql);
-        $stmt->execute([$user,$password,now()]);
+        try{
+            $stmt = pdo()->prepare($sql);
+            $password = password_hash($password, PASSWORD_DEFAULT);
+            $stmt->execute([$user,$password,now()]);
+            return [
+                'errCode' => 0,
+                'status' => '註冊成功'
+            ];
+        }catch(PDOException $e){
+            echo $e->getMessage();
+            return [
+                'errCode' => 1,
+                'status' => '註冊失敗'
+            ];
+        }
     }
