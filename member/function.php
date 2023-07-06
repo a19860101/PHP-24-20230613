e472b30e28c8399e7c013f5e0579f620072d5637
Zac <a19860101@gmail.com>
2023-07-06 20:55:25 +0800

    20230706

--- member/function.php
+++ member/function.php
@@ -4,6 +4,9 @@
     function store($request){
         extract($request);
         $sql = 'INSERT INTO users(user,password,created_at)VALUES(?,?,?)';
+        if(chkUser($user) != 0){
+            return chkUser($user);
+        }
         try{
             $stmt = pdo()->prepare($sql);
             $password = password_hash($password, PASSWORD_DEFAULT);
@@ -18,5 +21,18 @@
                 'errCode' => 1,
                 'status' => '註冊失敗'
             ];
+        }
+    }
+    function chkUser($user){
+        $sql = 'SELECT * FROM users WHERE user = ?';
+        $stmt = pdo()->prepare($sql);
+        $stmt->execute([$user]);
+        if($stmt->rowCount() > 0){
+            return [
+                'errCode' => 2,
+                'status' => '帳號重複，請重新註冊'
+            ];
+        }else{
+            return 0;
         }
     }
