dc2d1ab6e5c1f9a7a443dbc88a3e67055f41b161
Zac <a19860101@gmail.com>
2023-07-06 21:58:39 +0800

    20230706

--- member/logout.php
+++ member/logout.php
@@ -1,5 +1,6 @@
 <?php
     session_start();
-    session_destroy();
+    // session_destroy();
+    unset($_SESSION['AUTH']);
     echo "<script>alert('已登出')</script>";
     header('refresh:0;url=index.php');
