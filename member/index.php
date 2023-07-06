7fcd8be0325bf09f00bdc2bc7db7e0b42f170867
Zac <a19860101@gmail.com>
2023-07-06 20:02:09 +0800

    20230706

---
+++ member/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <a href="index.php">logo</a>
+        <div>
+            <a href="login.php">登入</a>
+            <a href="register.php">註冊</a>
+            <a href="logout.php">登出</a>
+        </div>
+    </nav>
+</body>
+</html>
---
+++ member/register.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>註冊</title>
+</head>
+<body>
+    <nav>
+        <a href="index.php">logo</a>
+        <div>
+            <a href="login.php">登入</a>
+            <a href="register.php">註冊</a>
+            <a href="logout.php">登出</a>
+        </div>
+    </nav>
+    <h1>註冊</h1>
+    <form action="store.php" method="post">
+        <div>
+            <label for="">帳號</label>
+            <input type="text" name="user">
+        </div>
+        <div>
+            <label for="">密碼</label>
+            <input type="password" name="password">
+        </div>
+        <input type="submit" value="註冊">
+    </form>
+</body>
+</html>
