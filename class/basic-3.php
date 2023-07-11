c380a6b3beef769b706b4e22828c609be18fb929
Zac <a19860101@gmail.com>
2023-07-11 20:23:29 +0800

    20230711

--- class/basic-3.php
+++ class/basic-3.php
@@ -1,10 +1,17 @@
 <?php
     class User {
         public $name = 'JohnDoe';
+        // 建構子
+        public function __construct(){
+            echo '我是建構子';
+        }
         //靜態方法(不可使用$this)
         static function test($name){
             return $name.'TEST';
         }
     }
 
-    echo User::test('QQQ');
+    // echo User::test('QQQ');
+
+    $user_1 = new User;
+    $user_2 = new User;
