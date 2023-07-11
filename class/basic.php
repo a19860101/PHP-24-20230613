1cd14bec9b7ed06862cfcc6fa3f768e2d0cd1dc0
Zac <a19860101@gmail.com>
2023-07-11 19:18:28 +0800

    20230711

--- class/basic.php
+++ class/basic.php
@@ -2,7 +2,7 @@
     // 類別
     class User {
         // 屬性
-        public $name;
+        public $name = 'JohnDoe';
         public $gender;
         // 方法
         public function walk(){
@@ -15,6 +15,9 @@
 
     // 建立物件（實體）
     $user_1 = new User;
+    $user_1->name = 'Max';
+    $user_1->gender = 'Male';
+    // echo $user_1->name;
 
     var_dump($user_1);
 
