bd90c6bf39567838ee87dd48423b5ac3210b89d7
Zac <a19860101@gmail.com>
2023-07-11 19:25:30 +0800

    20230711

--- class/basic.php
+++ class/basic.php
@@ -6,18 +6,20 @@
         public $gender;
         // 方法
         public function walk(){
-            return 'walk';
+            return $this->name.'walk';
         }
         public function fly(){
-            return 'fly';
+            return $this->name.'fly';
         }
     }
 
     // 建立物件（實體）
     $user_1 = new User;
-    $user_1->name = 'Max';
+    // $user_1->name = 'Max';
     $user_1->gender = 'Male';
+    echo $user_1->walk();
+    echo $user_1->fly();
     // echo $user_1->name;
 
-    var_dump($user_1);
+    // var_dump($user_1);
 
