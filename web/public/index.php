c826e1d9c2c4d7068edce4480e5cc650f79c6fff
Zac <a19860101@gmail.com>
2023-07-13 19:57:36 +0800

    20230713

--- web/public/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-    include('../vendor/autoload.php');
-    use App\Web\Config\DB;
-
-    echo DB::now();
---
+++ web/src/Controller/Post.php
@@ -0,0 +1,26 @@
+<?php
+    namespace App\Web\Controller;
+    use App\Web\Config\DB;
+
+    class Post {
+        static function index(){
+            $sql = 'SELECT * FROM posts';
+            // $posts = App\Web\Config\DB::pdo();
+            $posts = DB::pdo()->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+            return $posts;
+        }
+        static function show($request){
+            extract($request);
+            $sql = 'SELECT * FROM posts WHERE id = ?';
+            $stmt = DB::pdo()->prepare($sql);
+            $stmt->execute([$id]);
+            $post = $stmt->fetch(PDO::FETCH_ASSOC);
+            return $post;
+        }
+        static function store($request){
+            extract($request);
+            $sql = 'INSERT INTO posts (title,body,category_id,created_at)VALUES(?,?,?,?)';
+            $stmt = DB::pdo()->prepare($sql);
+            $stmt->execute([$title, $body, $category_id, DB::now()]);
+        }
+    }
