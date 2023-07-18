ced8561a682202f8c6cbc6c0d1352973c44ffb6d
Zac <a19860101@gmail.com>
2023-07-18 21:02:47 +0800

    20230718

--- web/public/category/index.php
+++ web/public/category/index.php
@@ -1,3 +1,8 @@
+<?php
+    include('../../vendor/autoload.php');
+    use App\Web\Controller\Category;
+    $categories = Category::index();
+?>
 <?php include('../template/header.php'); ?>
 <?php include('../template/nav.php'); ?>
 <div class="container mx-auto py-5 flex flex-wrap">
@@ -18,10 +23,14 @@
     </div>
     <div class="w-1/3 p-5">
         <ul class="divide-y divide-blue-200">
-            <li class="p-3">liste</li>
-            <li class="p-3">liste</li>
-            <li class="p-3">liste</li>
-            <li class="p-3">liste</li>
+            <?php foreach($categories as $category){ ?>
+            <li class="p-3 flex justify-between items-center"><?php echo $category['title'];?>
+                <form action="">
+                    <input type="hidden" value="<?php echo $category['id'];?>" name="id">
+                    <input type="submit" value="刪除" class="bg-red-600 text-white inline-block px-4 py-1 rounded-lg">
+                </form>               
+            </li>
+            <?php } ?>
         </ul>
     </div>
 </div>
