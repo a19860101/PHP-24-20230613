8bef3000d975564e9798e95835119b148255122a
Zac <a19860101@gmail.com>
2023-07-20 21:31:08 +0800

    20230720

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -22,6 +22,11 @@ Route::get('/',function(){
     return view('home');
 });
 
+Route::get('/post',function(){
+    // return view('post/index');
+    return view('post.index');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
