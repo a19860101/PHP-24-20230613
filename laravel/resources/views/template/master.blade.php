fd748022317351269db0d104ff7dd81b641c0e81
Zac <a19860101@gmail.com>
2023-08-03 19:40:56 +0800

    20230803

--- laravel/resources/views/template/master.blade.php
+++ laravel/resources/views/template/master.blade.php
@@ -5,16 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
 <body>
-    <nav>
-        <a href="{{route('article.index')}}">文章首頁</a>
-        <a href="{{route('article.create')}}">新增文章</a>
-    </nav>
+    <nav class="navbar navbar-dark navbar-expand-lg bg-dark mb-5">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="{{route('article.index')}}">Logo</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="{{route('article.index')}}">文章首頁</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{route('article.create')}}">新增文章</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     @yield('main')
     @yield('side')
-    <footer>
-        I AM FOOTER
-    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
 </html>
