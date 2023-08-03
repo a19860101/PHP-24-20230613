3fb4ad3af1133d3f96dbb7717af496c8547816f9
Zac <a19860101@gmail.com>
2023-08-03 21:48:34 +0800

    20230803

--- laravel/app/Models/Article.php
+++ laravel/app/Models/Article.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory;
-    protected $fillable = ['title','body','cover'];
+    protected $fillable = ['title','body','cover','category_id'];
 }
