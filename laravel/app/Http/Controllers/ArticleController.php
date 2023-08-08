a1b774745b89ea43985523db7b27f95709607102
Zac <a19860101@gmail.com>
2023-08-08 20:29:49 +0800

    20230808

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -106,7 +106,8 @@ class ArticleController extends Controller
     public function edit(Article $article)
     {
         //
-        return view('article.edit',compact('article'));
+        $categories = Category::get();
+        return view('article.edit',compact('article','categories'));
     }
 
     /**
--- laravel/resources/views/article/edit.blade.php
+++ laravel/resources/views/article/edit.blade.php
@@ -1,24 +1,43 @@
 @extends('template.master')
 @section('main')
+<div class="container">
+    <div class="row">
+        <div class="col-8">
+
+
     <h1>編輯文章</h1>
     <div>
         <form action="{{route('article.update',$article->id)}}" method="post">
             @csrf
             @method('patch')
-            <div>
-                <label for="">文章標題</label>
-                <input type="text" name="title" value="{{$article->title}}">
+            <div class="mb-3">
+                <label for="" class='form-label'>文章標題</label>
+                <input type="text" name="title" value="{{$article->title}}" class="form-control">
             </div>
-            <div>
-                <label for="">內文</label>
-                <textarea name="body" id="" cols="30" rows="10">{{$article->body}}</textarea>
+            <div class="mb-3">
+                <label for="" class='form-label'>分類</label>
+                <select name="category_id" id="" class="form-control">
+                    <option value='null'>--請選擇--</option>
+                    @foreach($categories as $category)
+                    <option value="{{$category->id}}" @if($category->id==$article->category_id) selected @endif>
+                        {{$category->title}}
+                    </option>
+                    @endforeach
+                </select>
             </div>
-            <input type="submit" value="儲存文章">
-            <input type="button" value="取消" onclick="history.back()">
+            <div class="mb-3">
+                <label for="" class='form-label'>內文</label>
+                <textarea name="body" id="" cols="30" rows="10" class="form-control">{{$article->body}}</textarea>
+            </div>
+            <input type="submit" value="儲存文章" class="btn btn-primary">
+            <input type="button" value="取消" onclick="history.back()" class="btn btn-danger">
         </form>
-        <a href="{{route('article.index')}}">文章列表</a>
+        <a href="{{route('article.index')}}" class="btn btn-success">文章列表</a>
     </div>
+</div>
+</div>
+</div>
     @endsection
     @section('side')
     <h1>side</h1>
-    @endsection
+@endsection
